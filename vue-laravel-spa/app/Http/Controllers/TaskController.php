9e81d288249dd96d009ac13eee7c88054e1f55b1
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-10 06:48:32 +0900

    タスク削除API実装

--- vue-laravel-spa/app/Http/Controllers/TaskController.php
+++ vue-laravel-spa/app/Http/Controllers/TaskController.php
@@ -7,11 +7,32 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return Task::all();
     }
 
-    public function show(Task $task) {
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return $task;
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
         return $task;
     }
 }
--- vue-laravel-spa/routes/api.php
+++ vue-laravel-spa/routes/api.php
@@ -19,4 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
+Route::put('/tasks/{task}', 'TaskController@update');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
